930dfac6b54bf54db92d8be51a6f3fea33912a3f
Sumit Jain <sjain5_be17@thapar.edu>
2020-09-28 22:03:44 +0530

    make it better

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+import os
 
 STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
-import os
 
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
